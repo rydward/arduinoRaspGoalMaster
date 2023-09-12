295a06db5a31722a8c6b776c19978b042882ae54
Lucas Lemal <lucas.lemal2002@gmail.com>
2023-09-12 23:32:30 +0200

    ajout led recherche de match

--- code arduino/arduino/arduino.ino
+++ code arduino/arduino/arduino.ino
@@ -1,21 +1,38 @@
 int joueur1 = 2; // Port d'entrée numérique
 int joueur2 = 3; // Port d'entrée numérique
+
 void setup() {
-pinMode(joueur1, INPUT); // Initialiser le port d'entrée numérique en mode entrée
-pinMode(joueur2, INPUT); // Initialiser le port d'entrée numérique en mode entrée
-Serial.begin(9600); // Initialiser la communication série
+  pinMode(joueur1, INPUT); // Initialiser le port d'entrée numérique en mode entrée
+  pinMode(joueur2, INPUT); // Initialiser le port d'entrée numérique en mode entrée
+  Serial.begin(9600); // Initialiser la communication série
 }
 
 void loop() {
-int etatJoueur1 = digitalRead(joueur1); // Lire l'état du bouton poussoir
-int etatJoueur2 = digitalRead(joueur2); // Lire l'état du bouton poussoir
+  int etatJoueur1 = digitalRead(joueur1); // Lire l'état du bouton poussoir
+  int etatJoueur2 = digitalRead(joueur2); // Lire l'état du bouton poussoir
 
-if (etatJoueur2 == HIGH) {
-  Serial.println("joueur2 à marqué");
-  delay(1500);
-  }
-if (etatJoueur1 == HIGH) {
-  Serial.println("joueur1 à marqué");
-  delay(1500);
-  }
+  Serial.print("initialisation ok ");
+
+    // Lire les données depuis le port série
+    String receivedData = Serial.readString();
+
+    // Faites quelque chose avec les données reçues
+    Serial.print("Données reçues : ");
+    Serial.println(receivedData);
+    if (receivedData == "NOMATCH"){
+      digitalWrite(22,HIGH);
+      delay(1000);
+      digitalWrite(22,LOW);
+    }else{
+      digitalWrite(22,HIGH);
+    }
+
+  if (etatJoueur2 == HIGH) {
+    Serial.println("joueur2 à marqué");
+    delay(1500);
+    }
+  if (etatJoueur1 == HIGH) {
+    Serial.println("joueur1 à marqué");
+    delay(1500);
+    }
 }
